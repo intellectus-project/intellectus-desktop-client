a9b7dca0f2352321b9569624cd791af55166d8b5
fnagelkop <feru.nagel@gmail.com>
2020-07-25 22:00:50 -0300

    on-call suggestions mock view

---
+++ intellectus-desktop-client/Suggestions/Suggestions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace intellectus_desktop_client
+{
+    public partial class Suggestions : Form
+    {
+        public Suggestions()
+        {
+            InitializeComponent();
+            lblTranscurredTimeName.Text = "Tiempo transcurrido:";
+        }
+    }
+}
