8ebaa64c75b4bf3d5c16910530864821fa75a059
fnagelkop <feru.nagel@gmail.com>
2020-07-25 22:11:58 -0300

    On-call window rename

---
+++ intellectus-desktop-client/Suggestions/OnCallWindow.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace intellectus_desktop_client
+{
+    partial class OnCallWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblConsultantName = new System.Windows.Forms.Label();
+            this.lblVoiceOrigin = new System.Windows.Forms.Label();
+            this.lblTranscurredTimeName = new System.Windows.Forms.Label();
+            this.lblTranscurredTime = new System.Windows.Forms.Label();
+            this.btnEndCall = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblOperatorName = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblConsultantName
+            // 
+            this.lblConsultantName.AccessibleName = "lblConsultantName";
+            this.lblConsultantName.AutoSize = true;
+            this.lblConsultantName.Location = new System.Drawing.Point(82, 38);
+            this.lblConsultantName.Name = "lblConsultantName";
+            this.lblConsultantName.Size = new System.Drawing.Size(53, 15);
+            this.lblConsultantName.TabIndex = 0;
+            this.lblConsultantName.Text = "Pepa pig";
+            // 
+            // lblVoiceOrigin
+            // 
+            this.lblVoiceOrigin.AccessibleName = "lblVoiceOrigin";
+            this.lblVoiceOrigin.AutoSize = true;
+            this.lblVoiceOrigin.Location = new System.Drawing.Point(82, 85);
+            this.lblVoiceOrigin.Name = "lblVoiceOrigin";
+            this.lblVoiceOrigin.Size = new System.Drawing.Size(75, 15);
+            this.lblVoiceOrigin.TabIndex = 1;
+            this.lblVoiceOrigin.Text = "Google meet";
+            // 
+            // lblTranscurredTimeName
+            // 
+            this.lblTranscurredTimeName.AutoSize = true;
+            this.lblTranscurredTimeName.Location = new System.Drawing.Point(50, 146);
+            this.lblTranscurredTimeName.Name = "lblTranscurredTimeName";
+            this.lblTranscurredTimeName.Size = new System.Drawing.Size(117, 15);
+            this.lblTranscurredTimeName.TabIndex = 2;
+            this.lblTranscurredTimeName.Text = "Tiempo transcurrido:";
+            // 
+            // lblTranscurredTime
+            // 
+            this.lblTranscurredTime.AccessibleName = "transcurredTime";
+            this.lblTranscurredTime.AutoSize = true;
+            this.lblTranscurredTime.Location = new System.Drawing.Point(168, 146);
+            this.lblTranscurredTime.Name = "lblTranscurredTime";
+            this.lblTranscurredTime.Size = new System.Drawing.Size(34, 15);
+            this.lblTranscurredTime.TabIndex = 3;
+            this.lblTranscurredTime.Text = "00:00";
+            // 
+            // btnEndCall
+            // 
+            this.btnEndCall.AccessibleName = "btnEndCall";
+            this.btnEndCall.Location = new System.Drawing.Point(82, 199);
+            this.btnEndCall.Name = "btnEndCall";
+            this.btnEndCall.Size = new System.Drawing.Size(75, 23);
+            this.btnEndCall.TabIndex = 4;
+            this.btnEndCall.Text = "Finalizar";
+            this.btnEndCall.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(229, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1, 305);
+            this.label1.TabIndex = 5;
+            // 
+            // lblOperatorName
+            // 
+            this.lblOperatorName.AccessibleName = "lblOperatorName";
+            this.lblOperatorName.AutoSize = true;
+            this.lblOperatorName.Location = new System.Drawing.Point(484, 20);
+            this.lblOperatorName.Name = "lblOperatorName";
+            this.lblOperatorName.Size = new System.Drawing.Size(62, 15);
+            this.lblOperatorName.TabIndex = 6;
+            this.lblOperatorName.Text = "Juan Perez";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AccessibleName = "currentSuggestion";
+            this.textBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox1.Location = new System.Drawing.Point(327, 119);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 16);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.Text = "Bla";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.ForeColor = System.Drawing.Color.Gray;
+            this.textBox2.Location = new System.Drawing.Point(327, 159);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 16);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "blaViejo";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.ForeColor = System.Drawing.Color.Silver;
+            this.textBox3.Location = new System.Drawing.Point(327, 199);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 16);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "blaAnterior";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Suggestions
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblOperatorName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEndCall);
+            this.Controls.Add(this.lblTranscurredTime);
+            this.Controls.Add(this.lblTranscurredTimeName);
+            this.Controls.Add(this.lblConsultantName);
+            this.Controls.Add(this.lblVoiceOrigin);
+            this.Name = "Suggestions";
+            this.Text = "Suggestions";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblConsultantName;
+        private System.Windows.Forms.Label lblVoiceOrigin;
+        private System.Windows.Forms.Label lblTranscurredTimeName;
+        private System.Windows.Forms.Label lblTranscurredTime;
+        private System.Windows.Forms.Button btnEndCall;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblOperatorName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
