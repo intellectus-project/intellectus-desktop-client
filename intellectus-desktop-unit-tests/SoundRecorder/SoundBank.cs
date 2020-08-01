290d955da9d15ea98dcbc6d73bfbb688a48b4e29
Ronan <avinitzca@gmail.com>
2020-08-01 19:22:17 -0300

    Added missing file

--- intellectus-desktop-unit-tests/SoundRecorder/SoundBank.cs
+++ intellectus-desktop-unit-tests/SoundRecorder/SoundBank.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,24 +12,38 @@ namespace intellectus_desktop_unit_tests
     {
         private string projectDirectory, pathToSoundBank, pathToTemp;
         private List<string> sounds;
-        
+
 
         public SoundBank()
         {
-            var enviroment = Environment.CurrentDirectory;
-            projectDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+            projectDirectory = FindProjectDirectory();
             pathToSoundBank = projectDirectory + SoundBankRelativeLocation;
             pathToTemp = projectDirectory + TempFolderRelativeLocation;
 
             if (!Directory.Exists(pathToSoundBank))
                 Directory.CreateDirectory(pathToSoundBank);
-            
+
             if (!Directory.Exists(pathToTemp))
                 Directory.CreateDirectory(pathToTemp);
 
             sounds = new List<string>(Directory.EnumerateFiles(PathToSoundBank));
             if (sounds.Count == 0)
                 throw new FileNotFoundException("Sound bank is empty");
+        }
+
+        private string FindProjectDirectory()
+        {
+            var rootDirectory = Assembly.GetExecutingAssembly().GetName().Name;
+            var environment = Environment.CurrentDirectory;
+
+            var actual = new DirectoryInfo(environment);
+            while (actual.Name.CompareTo(rootDirectory) != 0)
+            {
+                actual = actual.Parent;
+                if (actual == null)
+                    throw new DirectoryNotFoundException("Cannot find the project root");
+            }
+            return actual.FullName;
         }
 
 
@@ -43,14 +58,14 @@ namespace intellectus_desktop_unit_tests
 
         private static SoundBank instance;
 
-        public static SoundBank Instance 
+        public static SoundBank Instance
         {
             get
             {
                 if (instance == null)
                     instance = new SoundBank();
                 return instance;
-            }            
+            }
         }
 
 
