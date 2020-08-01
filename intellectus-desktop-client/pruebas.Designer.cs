bf03535670adc5caf80c2db6b41c22cff0289848
fnagelkop <feru.nagel@gmail.com>
2020-08-01 11:41:48 -0300

    + suggestion windows trial

---
+++ intellectus-desktop-client/pruebas.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace intellectus_desktop_client
+{
+    partial class pruebas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sugerencia = new System.Windows.Forms.TextBox();
+            this.send = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // sugerencia
+            // 
+            this.sugerencia.Location = new System.Drawing.Point(232, 65);
+            this.sugerencia.Name = "sugerencia";
+            this.sugerencia.Size = new System.Drawing.Size(100, 23);
+            this.sugerencia.TabIndex = 0;
+            // 
+            // send
+            // 
+            this.send.Location = new System.Drawing.Point(377, 65);
+            this.send.Name = "send";
+            this.send.Size = new System.Drawing.Size(75, 23);
+            this.send.TabIndex = 1;
+            this.send.Text = "button1";
+            this.send.UseVisualStyleBackColor = true;
+            this.send.Click += new System.EventHandler(this.send_Click);
+            // 
+            // pruebas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.send);
+            this.Controls.Add(this.sugerencia);
+            this.Name = "pruebas";
+            this.Text = "pruebas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox sugerencia;
+        private System.Windows.Forms.Button send;
+    }
+}
---
+++ intellectus-desktop-client/pruebas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace intellectus_desktop_client
+{
+    public partial class pruebas : Form
+    {
+        public pruebas()
+        {
+            InitializeComponent();
+        }
+
+        private void send_Click(object sender, EventArgs e)
+        {
+            OnCallWindow onCallWindow = (OnCallWindow)Application.OpenForms["OnCallWindow"];
+            onCallWindow.Suggest(sugerencia.Text);
+        }
+    }
+}
