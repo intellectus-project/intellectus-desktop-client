98bc938908b21e6aa6c1c2771e610e6e4de8adb9
fnagelkop <feru.nagel@gmail.com>
2020-08-17 00:12:51 -0300

    change stats to EmotionProbabilites at Call class - comment recording service class, needs new implementation of the recorder

--- intellectus-desktop-client/Services/Recording.cs
+++ intellectus-desktop-client/Services/Recording.cs
@@ -8,40 +8,40 @@ namespace intellectus_desktop_client.Services
 {
     public static class Recording
     {
-        public static ISoundRecorder OperatorRecorder;
-        public static ISoundRecorder ConsultantRecorder;
+        public static ISoundSource OperatorRecorder;
+        public static ISoundSource ConsultantRecorder;
         public static string operatorName = "Pablo Fernandez";
         public static ISoundListener operatorWriter;
         public static ISoundListener consultantWriter;
 
         public static void StartRecording()
         {
-            InitializeOperatorRecorder();
+           /* InitializeOperatorRecorder();
             InitializeConsultantRecorder();
             ConsultantRecorder.Start();
-            OperatorRecorder.Start();
+            OperatorRecorder.Start();*/
         }
 
 
         public static void StopRecording()
         {
-            ConsultantRecorder.Stop();
-            OperatorRecorder.Stop();
+           /* ConsultantRecorder.Stop();
+            OperatorRecorder.Stop();*/
         }
 
         private static void InitializeOperatorRecorder()
         {
-            OperatorRecorder = new InputSoundRecorder();
+           /* OperatorRecorder = new InputSoundRecorder();
             OperatorRecorder.Configure(0, new NAudio.Wave.WaveFormat(44100, 1));
             operatorWriter = new SoundFileWriterListener(string.Format("{0}/Grabaciones/operator_{1}_{2}.wav", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), operatorName, DateTime.Now.ToFileTimeUtc()), OperatorRecorder.GetWaveFormat());
-            OperatorRecorder.AddListener(operatorWriter);
+            OperatorRecorder.AddListener(operatorWriter);*/
         }
         private static void InitializeConsultantRecorder()
         {
-            ConsultantRecorder = new OutputSoundRecorder();
+           /* ConsultantRecorder = new OutputSoundRecorder();
             ConsultantRecorder.Configure(0, new NAudio.Wave.WaveFormat(44100, 1));
             consultantWriter = new SoundFileWriterListener(string.Format("{0}/Grabaciones/consultant_{1}_{2}.wav", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), operatorName, DateTime.Now.ToFileTimeUtc()), ConsultantRecorder.GetWaveFormat());
-            ConsultantRecorder.AddListener(consultantWriter);
+            ConsultantRecorder.AddListener(consultantWriter);*/
         }
     }
 }
