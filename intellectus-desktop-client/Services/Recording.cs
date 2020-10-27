95016238fb0730206297e0068ef7df8e7b0290a7
Ronan <avinitzca@gmail.com>
2020-10-27 01:28:22 -0300

    Separated API URI paths, saved recording timestamps for files

--- intellectus-desktop-client/Services/Recording.cs
+++ intellectus-desktop-client/Services/Recording.cs
@@ -6,23 +6,32 @@ using Suggestions;
 using Suggestions.Systems;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace intellectus_desktop_client.Services
 {
     public static class Recording
     {
+        private readonly static string RecordingsPath = "/Grabaciones/";
+
         public static ISoundSource OperatorRecorder;
         public static ISoundSource ConsultantRecorder;
         public static string operatorName = "Pablo Fernandez";
-        public static ISoundListener OperatorWriter;
-        public static ISoundListener ConsultantWriter;
+
+        public static SoundFileWriter OperatorWriter;
+        public static SoundFileWriter ConsultantWriter;
+
+        private static string operatorTimestamp;
+        private static string consultantTimestamp;
 
         private static SuggestionsSystem suggestionsSystem;
         private static VokaturiSingleExtractor operatorSingleExtractor; 
 
         public static void StartRecording(ISuggestionsListener suggestionListener)
         {
+            CreateRecordingsPath();
+
             InitializeOperatorRecorder(suggestionListener);
             InitializeConsultantRecorder();
 
@@ -35,13 +44,17 @@ namespace intellectus_desktop_client.Services
         {
             ConsultantRecorder.Stop();
             OperatorRecorder.Stop();
+            OperatorWriter.Close();
+            ConsultantWriter.Close();
         }
 
         private static void InitializeOperatorRecorder(ISuggestionsListener suggestionListener)
         {
             var waveFormat = new NAudio.Wave.WaveFormat(44100, 1);
             OperatorRecorder = new InputSoundSource(waveFormat, 1);
-            OperatorWriter = new SoundFileWriter(FormatPath("/Grabaciones/operator", operatorName), waveFormat);
+
+            operatorTimestamp = DateTime.Now.ToFileTimeUtc().ToString();
+            OperatorWriter = new SoundFileWriter(FormatPath("operator", operatorName, operatorTimestamp), waveFormat);
             var voiceListener = new VoiceListener(waveFormat, 10f);
 
             OperatorRecorder.AddListener(OperatorWriter);
@@ -59,13 +72,15 @@ namespace intellectus_desktop_client.Services
         private static void InitializeConsultantRecorder()
         {
             ConsultantRecorder = new OutputSoundSource();
-            ConsultantWriter = new SoundFileWriter(FormatPath("/Grabaciones/consultant"), ConsultantRecorder.GetWaveFormat());
+
+            consultantTimestamp = DateTime.Now.ToFileTimeUtc().ToString();
+            ConsultantWriter = new SoundFileWriter(FormatPath("consultant", consultantTimestamp), ConsultantRecorder.GetWaveFormat());
             ConsultantRecorder.AddListener(ConsultantWriter);
         }
 
         public static EmotionsProbabilities ExtractOperatorEmotions()
         {
-            string sourcePath = FormatPath("/Grabaciones/operator", operatorName);
+            string sourcePath = FormatPath("operator", operatorName, operatorTimestamp);
             var input = new FileSoundSource(sourcePath);
 
             float seconds = (float)input.FileSize / input.GetWaveFormat().AverageBytesPerSecond;
@@ -78,13 +93,14 @@ namespace intellectus_desktop_client.Services
             input.AddListener(listener);
             input.Start();
             input.Stop();
+
 
             return extractor.Extraction.Emotions;
         }
 
         public static EmotionsProbabilities ExtractConsultantEmotions()
         {
-            string sourcePath = FormatPath("/Grabaciones/consultant");
+            string sourcePath = FormatPath("consultant", consultantTimestamp);
             var input = new FileSoundSource(sourcePath);
 
             float seconds = (float)input.FileSize / input.GetWaveFormat().AverageBytesPerSecond;
@@ -101,20 +117,27 @@ namespace intellectus_desktop_client.Services
             return extractor.Extraction.Emotions;
         }
 
+        private static void CreateRecordingsPath()
+        {
+            var path = DirectoryPath();
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+        }
 
+        private static string DirectoryPath() => Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + RecordingsPath;
 
         public static float Rating => suggestionsSystem.Rating();
 
         public static EmotionsProbabilities OperatorLastStats => operatorSingleExtractor.Extraction.Emotions;
 
-        private static string FormatPath(string basePath)
+        private static string FormatPath(string baseFileName, string timestamp)
         {
-            return string.Format("{0}" + basePath + "_{1}.wav", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), DateTime.Now.ToFileTimeUtc());
+            return string.Format("{0}{1}_{2}.wav", DirectoryPath(), baseFileName, timestamp);
         }
 
-        private static string FormatPath(string basePath, string name)
+        private static string FormatPath(string baseFileName, string name, string timestamp)
         {
-            return string.Format("{0}" + basePath + "_{1}_{2}.wav", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), name, DateTime.Now.ToFileTimeUtc());
+            return string.Format("{0}{1}_{2}_{3}.wav",  DirectoryPath(), baseFileName, name, timestamp);
         }
 
 
