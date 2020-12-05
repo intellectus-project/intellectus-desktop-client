9b2544c0dddd76d70208ecb7346c20f754f98c8a
Ronan <avinitzca@gmail.com>
2020-12-04 23:00:41 -0300

    Added backgrounds and adjusted styles

--- intellectus-wpf-client/Views/RelaxationWindow.xaml.cs
+++ intellectus-wpf-client/Views/RelaxationWindow.xaml.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 namespace intellectus_wpf_client
@@ -17,13 +10,53 @@ namespace intellectus_wpf_client
     /// </summary>
     public partial class RelaxationWindow : Window
     {
+        private const string RelaxationVideosRelativePath = @"\Resources\Relaxation\";
+        private const string RelaxationVideosFileType = ".mp4";
+        
+        private bool Paused = false;
+
+
         public RelaxationWindow()
         {
             InitializeComponent();
 
-            mediaVideoPlayer.Source = new Uri("/Resources/Relaxation/1.mp4", UriKind.Relative);
+
+            var pathToExecuting = Directory.GetCurrentDirectory().ToString();
+            var totalPath = pathToExecuting + RelaxationVideosRelativePath;
+            var files = Directory.GetFiles(totalPath);
+
+
+            Random random = new Random();
+            var index = random.Next(0, files.Length);
+            
+            Uri outUri;
+            Uri.TryCreate(files[index], UriKind.Relative, out outUri);
+            mediaVideoPlayer.Source = outUri;
             mediaVideoPlayer.Play();
         }
 
+
+        private void btnReturn_Click(object sender, RoutedEventArgs e)
+        {
+            EnteringCallWindow enteringCall = new EnteringCallWindow();
+            enteringCall.Show();
+            Close();
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            Paused = !Paused;
+            if(Paused)
+            {
+                mediaVideoPlayer.Pause();
+                btnStop.Content = "Reproducir";
+            }
+            else
+            {
+                mediaVideoPlayer.Play();
+                btnStop.Content = "Pausar";
+            }
+            
+        }
     }
 }
